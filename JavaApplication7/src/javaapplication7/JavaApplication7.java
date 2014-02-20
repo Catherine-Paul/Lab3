933d0d432ea4c21f0b473b9c1146d97bec787c7b
Catherine <cthrnpaul@gmail.com>
2014-02-19 23:50:29 -0600

    Serializer Code added for Media and some Menu Items(cp)

---
+++ JavaApplication7/src/javaapplication7/JavaApplication7.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2014 Ballantine Communications, Inc., a Colorado corporation
+ * Describe requirements this class relates to.
+ * 0.0.0.000: Catherine Paul <at BDMedia> Feb 19, 2014 Initial file created
+ */
+
+package javaapplication7;
+
+/**
+ *
+ * @author Catherine Paul <at BDMedia>
+ */
+public class JavaApplication7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
